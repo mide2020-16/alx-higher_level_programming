1f4f2869c52bace0abb351005c92562d1432de84
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-18 15:10:38 -0800

    inifint 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    result = 0
+    argc = len(argv)
+    if argc == 0:
+        print("{}".format(0))
+    else:
+        for i in range(1, len(argv)):
+            result += int(argv[i])
+        print("{}".format(result))
