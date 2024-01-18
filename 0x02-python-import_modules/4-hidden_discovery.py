57cbc57d0786abe5e7e953a6f8582e97f55317b7
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-18 15:29:56 -0800

    hidden 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    argc = len(names)
+    for i in range(argc):
+        if "__" in names[i]:
+            continue
+        print(names[i])
