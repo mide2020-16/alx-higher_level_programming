25ed3a3243c8ad400e2d9562a4ff66ae570cde5a
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-25 20:45:43 +0100

    dsa

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    # Use dictionary comprehension to create a new dictionary with values multiplied by 2
+    # Use dictionary comprehension to create a new dictionary
     new_dict = {key: value * 2 for key, value in a_dictionary.items()}
     return new_dict
