b94faed3da5850223720a77937e8dc5dfaced0d6
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-09 12:14:16 +0100

    finally added

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
@@ -22,4 +20,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
-    return result
+        finally:
+            if i == list_length - 1:
+                return result
