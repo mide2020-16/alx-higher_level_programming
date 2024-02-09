fbfb3eda55cfad2e0cbfa90f7e6e0cf9852c1c6f
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-09 22:45:15 +0100

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Defines a class square"""
 class Square:
-        """A class called Squar that creates a square"""
+        """A class called Square that creates a square"""
         pass
