5da03cd442109efc68fad74c929ab293a01f71a4
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-09 14:01:25 -0800

    squares1

--- 0x06-python-classes/1-main.py
+++ 0x06-python-classes/1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+Square = __import__("1-square").Square
 
 my_square = Square(3)
 print(type(my_square))
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Defines a class square"""
+
+
 class Square:
-        """A class called Square that creates a square"""
-        def __init__(self, size):
-                """An initialization of the class Square"""
-                self.__size = size
+    """A class called Square that creates a square"""
+
+    def __init__(self, size):
+        """An initialization of the class Square"""
+        self.__size = size
