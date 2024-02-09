f460aaa5756441dddd219942dffc2653e4133840
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-09 22:58:08 +0100

    sqaure1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Defines a class square"""
+class Square:
+        """A class called Square that creates a square"""
+        def __init__(self, size):
+                """An initialization of the class Square"""
+                self.__size = size
