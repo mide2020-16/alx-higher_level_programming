4e450adbcc68426338b4ef53d30b967cf984cb14
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-22 20:04:05 +0100

    io

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-a script that adds all arguments to a Python list, and then save them to a file
+This script adds all arguments to a Python list, and then saves them to a file.
 """
-import json
-from sys import argv
+
+import json  # Importing the JSON module for handling JSON data
+from sys import argv  # Importing argv to access command line arguments
 
 
+# Importing the save_to_json_file function from 5-save_to_json_file.py
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+# Importing the load_from_json_file function from 6-load_from_json_file.py
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+# Opening the file 'add_item.json' in read and write mode ('r+')
 with open("add_item.json", 'r+') as f:
+    # Saving the command line arguments (excluding the script name) to 'add_item.json'
     save_to_json_file(argv[1:], "add_item.json")
