13b98b493eacd3cd576c965a30a0bdd74e3b6305
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-22 15:46:26 +0100

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,15 +5,17 @@
 def inherits_from(obj, a_class):
     """A function that checks if the class of an object
     is a subclass of another class
-    arguments:
-        obj(object) whose class is to be inspected
-        a_class(class) the class to be inspected
+
+    Args:
+        obj(object): whose class is to be inspected
+        a_class(class): the class to be inspected
+
     Return: True if the class of the object is a subclass of the specified
             class else false is returned
     """
     istance_class = type(obj)
+
     if issubclass(istance_class, a_class) and istance_class is not a_class:
         return True
     else:
         return False
-    
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """A rectangle instance"""
 
