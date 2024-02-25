e4ec32043093add568486ed21a8f0b5837d6c94f
mide2020-16 <efuwapeayomide51@gmail.com>
2024-02-26 00:32:39 +0100

    io

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,4 +14,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             f.write(line)
             if search_string in line:
-                f.write(new_string + '\n')
+                f.write(new_string)
