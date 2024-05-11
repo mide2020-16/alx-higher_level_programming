99aa464fed4be2fee2f22451a2b786dd9e0a3275
mide2020-16 <efuwapeayomide51@gmail.com>
2024-05-11 22:49:33 +0100

    Added body_size of a response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script takes URL as input and display the size of the body response
+
+if [ -z "$1"]; then
+	echo "Usage ./0-body_size.sh <URL>"
+	exit 1
+fi
+
+response=$(curl -s -o /tmp/body_size "$1")
+
+if [$? -ne 0]; then
+	echo "Error: Unable to get a response from $1"
+	exit 1
+fi
+
+body_size=$(stat -c %s /tmp/body_size)
+
+echo "$body_size"
